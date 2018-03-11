ef486c1dbd9d06054e7bab39b90fadf322b79742
Akramul Islam <akramul.i@agvcorp.com>
2018-03-11 13:01:11 +0600

    1st commit

---
+++ src/CalculatorController.php
@@ -0,0 +1,19 @@
+<?php
+namespace akashakram\calculator;
+use App\Http\Controllers;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class CalculatorController extends Controller
+{
+    public function add($a, $b){
+        $result = $a + $b;
+        return view('calculator::add', compact('result'));
+    }
+
+    public function subtract($a, $b){
+        $result = $a - $b;
+    return view('calculator::add', compact('result'));
+    }
+}
